e24832e2231612405f20edbd2c22f9c16921926c
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-01-25 13:53:52 +0100

    Adds image url to the schema

--- backend/src/models/project.js
+++ backend/src/models/project.js
@@ -5,6 +5,7 @@ const ProjectSchema = new mongoose.Schema(
     title: { type: String, required: true, trim: true },
     description: { type: String, default: "" },
     tech: [{ type: String, trim: true }],
+    imageUrl: { type: String },
     repoUrl: { type: String, default: "" },
     liveUrl: { type: String, default: "" },
   },
