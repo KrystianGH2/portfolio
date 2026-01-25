9af05887e224fc508ac66342423fec5d6eedec96
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-01-25 01:09:27 +0100

    Simple api route with express for fetching all data

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,20 @@ import { Project } from "./models/project.js";
 const app = express();
 app.use(express.json());
 
-app.get("api/projects", async (req, res) => {
+app.get("/api/projects", async (req, res) => {
   try {
     await connectDB();
-    const projects = Project.find().sort({ createdAt: -1 }).lean();
+
+    const projects = await Project.find().sort({ createdAt: -1 }).lean();
+
     res.json(projects);
-  } catch (error) {
-    res.status(500).json({ message: "Server Error", details: String(error) });
+  } catch (err) {
+    res.status(500).json({
+      message: "Server Error",
+      details: String(err),
+    });
   }
 });
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
