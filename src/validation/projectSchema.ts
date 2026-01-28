d85796e7e037a44e05767679d3ad236e09576148
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-01-28 21:04:12 +0100

    Validates the data with zod and makes a type of the schema

---
+++ src/validation/projectSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+export const projectSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  imageUrl: z.string().min(1, "Image URL must be a valid URL"),
+  tech: z.array(z.string().min(1).min(1, "Add at least one tech")),
+  repoUrl: z.string().url("Repo URL must be a valid URL"),
+  liveUrl: z.string().url("Live URL must be a valid URL"),
+});
+
+// Tuns this schema into a TS type
+export type ProjectTypes = z.infer<typeof projectSchema>;
