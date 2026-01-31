1595be96a038f79993e184787578bdeaf61d7f21
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-01-31 02:24:26 +0100

    Replaces the input with a textarea

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,17 @@
 "use client";
 import useCreateProject from "@/hooks/useCreateProject";
-
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 function Form() {
-  const { handleOnChange, handleOnSubmit, formData } = useCreateProject();
+  const { handleOnChange, handleOnSubmit, formData, handleTextArea, textArea } =
+    useCreateProject();
   return (
-    <div className="">
-      <form onSubmit={handleOnSubmit} className=" w-full">
+    <Card className="w-full m-auto sm:max-w-md p-8">
+      <h1 className="">Create project</h1>
+      <form className="flex flex-col gap-3" onSubmit={handleOnSubmit}>
         <div className="flex flex-col gap-2 justify-baseline items-start">
           <label> Title</label>
           <Input
@@ -18,14 +21,17 @@ function Form() {
             type="text"
           />
         </div>
-        <div className="flex flex-col gap-2 justify-baseline items-start">
+        <div className="flex flex-col relative gap-2 justify-baseline items-start">
           <label> Description</label>
-          <Input
-            onChange={handleOnChange}
+          <Textarea
+            className="h-30 align-text-top"
+            onChange={handleTextArea}
             name="description"
-            value={formData.description}
-            type="text"
+            value={textArea}
           />
+          <span className="text-gray-400 font-medium text-sm absolute left-2 bottom-2">
+            {formData.description.length} / 100 characters
+          </span>
         </div>
         <div className="flex flex-col gap-2 justify-baseline items-start">
           <label> Image Url</label>
@@ -64,11 +70,14 @@ function Form() {
           />
         </div>
 
-        <Button className="hover:text-accent hover:bg-accent" type="submit">
+        <Button
+          className="hover:text-accent hover:cursor-pointer mt-5"
+          type="submit"
+        >
           Create
         </Button>
       </form>
-    </div>
+    </Card>
   );
 }
 
