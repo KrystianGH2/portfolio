2490b7ee7152fba70907a257cb746cb4f33ef5f0
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-01-31 03:06:36 +0100

    Renders lists of the fetched projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 "use client";
-import "./App.css";
 import useProjects from "./hooks/useProjects";
 import Form from "./components/Form";
+import type { ProjectTypes } from "./validation/projectSchema";
+import "./index.css"
 
 function App() {
   const { projects } = useProjects();
 
   console.log(projects);
 
+  const lists = projects.map((item: ProjectTypes, index: number) => {
+    return (
+      <div className="flex flex-col items-start" key={index}>
+        <img
+          className="w-80"
+          src={item?.imageUrl}
+          alt={`${item.title} image`}
+        />
+        <h1>{item.title}</h1>
+      </div>
+    );
+  });
   return (
     <>
-      <div className="w-full border p-20 m-auto max-w-7xl">
+      <div className="w-full m-auto max-w-7xl">
+        <section>{lists}</section>
         <Form />
       </div>
     </>
