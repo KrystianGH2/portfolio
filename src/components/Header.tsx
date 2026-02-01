4caa791f5af2c96271de5af125727b230b4172b6
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-01 21:06:12 +0100

    Adds responsiveness to the header container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,19 +25,18 @@ function Header() {
           </ul>
         </section>
       </nav>
-
-      <header className="w-full flex flex-col lg:flex-row">
-        <section className="flex w-1/2 flex-col">
+      <header className="w-full flex flex-col justify-center lg:justify-evenly items-center md:flex-row">
+        <section className="flex lg:basis-1/2 flex-col">
           <div className="max-w-43">
             <span className=" border flex flex-row justify-between items-center gap-2 px-4 py-1 rounded-full text-xs font-medium tracking-wider">
               Available For Work{" "}
               <span
                 onChange={handleToggle}
-                className={`${availability ? "bg-green-500 " : "bg-red-500"} bg-green-500 w-2.5 h-2.5 block rounded-full animate-pulse animation`}
+                className={`${availability ? "bg-green-500 " : "bg-red-500"} bg-green-500 w-2.5 h-2.5 block rounded-full  animate-pulse`}
               ></span>
             </span>
           </div>
-          <h1 className="mt-5 font-bold text-4xl tracking-wide">
+          <h1 className="mt-5 font-bold text-5xl lg:text-6xl tracking-wide">
             Krystian Cruz
           </h1>
           <h2 className="mt-5 font-medium text-2xl tracking-wide">
@@ -57,14 +56,14 @@ function Header() {
             </button>
           </div>
         </section>
-        <section className="relative flex border flex-1/2 justify-center">
-          <div className="absolute top-12 right-0 bg-black h-10 w-10 rounded animate-pulse .float-header">
+        <section className="relative flex border w-full max-w-md lg:max-w-lg lg:basis-1/2 justify-center">
+          <div className="absolute top-12 right-0 bg-black h-10 w-10 rounded float-header-right">
             <span></span>
           </div>
-          <div className="bg-black h-60 w-70 m-15 rounded">
+          <div className="bg-black h-60 w-70 m-15 rounded float-header-middle">
             <span></span>
           </div>
-          <div className="  absolute bottom-12 left-0 bg-black h-10 w-10 rounded">
+          <div className="  absolute bottom-12 left-0 bg-black h-10 w-10 rounded float-header-left">
             <span></span>
           </div>
         </section>
