48b53cab935914351ba6397d9f431722afae3363
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-03 00:15:54 +0100

    Moves to code block object like styles to its own component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import CodeBlock from "./CodeBlock";
 
 const experienceCard = [
   { name: "2+ Years Learning", desc: "Front-end Development" },
@@ -14,7 +15,7 @@ const experienceCard = [
 
 const mappedCards = experienceCard.map((item) => {
   return (
-    <section className="text-center border p-4 text-2xl" key={item.name}>
+    <section className="text-center border p-4 md:text-2xl" key={item.name}>
       <CardContent>{item.name}</CardContent>
       <CardDescription>{item.desc}</CardDescription>
     </section>
@@ -24,7 +25,7 @@ const mappedCards = experienceCard.map((item) => {
 function About() {
   return (
     <div id="#about">
-      <Card className="flex justify-center items-center lg:flex-row">
+      <Card className="flex justify-center items-center lg:flex-row max-w-6xl">
         <section className=" basis-1/2">
           <CardHeader className="pl-">About me</CardHeader>
           <CardHeader>
@@ -34,7 +35,7 @@ function About() {
           </CardHeader>
 
           <CardContent>
-            <CardDescription>
+            <CardDescription className="mt-5">
               I’m a junior Front-End Developer with two years of hands-on
               experience through projects and continuous learning, building
               modern and responsive web applications. I focus on creating
@@ -45,58 +46,13 @@ function About() {
                 improving both my technical and personal skills.
               </p>
             </CardDescription>
-            <div className="flex justify-between flex-row p-4 md:p-0 gap-4">
+            <div className="flex justify-between mt-5 flex-row p-4 md:p-0 gap-4">
               {mappedCards}
             </div>
           </CardContent>
         </section>
-        <section className="md:basis-1/2">
-          <CardContent
-            className="
-    relative
-    rounded-2xl
-    bg-gradient-to-br from-indigo-950/80 to-slate-900/80
-    border border-white/10
-    p-6
-    font-mono
-    text-sm
-    leading-6
-    text-slate-200
-    shadow-xl
-    backdrop-blur-md
-  "
-          >
-            <pre className="space-y-1">
-              <div className="text-purple-400">const developer = &#123;</div>
-
-              <div className="pl-4">
-                <span className="text-sky-400">name</span>:{" "}
-                <span className="text-emerald-400">"Krystian Cruz"</span>,
-              </div>
-
-              <div className="pl-4">
-                <span className="text-sky-400">role</span>:{" "}
-                <span className="text-emerald-400">"Front-End Developer"</span>,
-              </div>
-
-              <div className="pl-4">
-                <span className="text-sky-400">loves</span>: [
-              </div>
-
-              <div className="pl-8 text-emerald-400">"Clean Code",</div>
-              <div className="pl-8 text-emerald-400">"Problem Solving",</div>
-              <div className="pl-8 text-emerald-400">"Building Products"</div>
-
-              <div className="pl-4">],</div>
-
-              <div className="pl-4">
-                <span className="text-sky-400">coffee</span>:{" "}
-                <span className="text-orange-400">true</span>,
-              </div>
-
-              <div className="text-purple-400">&#125;;</div>
-            </pre>
-          </CardContent>
+        <section className="w-full sm:basis-1/2">
+          <CodeBlock />
         </section>
       </Card>
     </div>
