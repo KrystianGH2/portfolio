cebaf1d0033b489f744845752fa586fbec598cef
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-03 23:52:45 +0100

    Adds styling to the cards section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,15 +8,18 @@ import {
 import CodeBlock from "./CodeBlock";
 
 const experienceCard = [
-  { name: "2+ Years Learning", desc: "Front-end Development" },
-  { name: "5+ Projects Built", desc: "Web Applications" },
-  { name: "React Focused", desc: "User Interface Development" },
+  { name: "2+", desc: "Years Learning" },
+  { name: "5+", desc: "Projects Built" },
+  { name: "React", desc: "Focused" },
 ];
 
 const mappedCards = experienceCard.map((item) => {
   return (
-    <section className="text-center border p-4 md:text-2xl" key={item.name}>
-      <CardContent>{item.name}</CardContent>
+    <section
+      className="text-center border rounded-md p-4 md:text-xl"
+      key={item.name}
+    >
+      <CardContent className="font-medium">{item.name}</CardContent>
       <CardDescription>{item.desc}</CardDescription>
     </section>
   );
@@ -24,8 +27,8 @@ const mappedCards = experienceCard.map((item) => {
 
 function About() {
   return (
-    <div id="#about">
-      <Card className="flex justify-center items-center lg:flex-row max-w-6xl">
+    <div id="about" className="m-auto max-w-6xl">
+      <Card className="flex justify-center lg:justify-between border-none  shadow-none items-center lg:flex-row max-w-6xl">
         <section className=" basis-1/2">
           <CardHeader className="pl-">About me</CardHeader>
           <CardHeader>
@@ -46,13 +49,17 @@ function About() {
                 improving both my technical and personal skills.
               </p>
             </CardDescription>
-            <div className="flex justify-between mt-5 flex-row p-4 md:p-0 gap-4">
+            <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-3">
               {mappedCards}
             </div>
           </CardContent>
         </section>
-        <section className="w-full sm:basis-1/2">
+        <section className="w-full lg:max-w-125  mt-10 lg:mt-2 relative  sm:basis-1/2">
           <CodeBlock />
+          <div
+            className="h-full absolute top-0 rounded-2xl z-0 bg-gradient-to-br opacity-50 from-indigo-950/80 to-slate-900/80
+    border border-white/10 rotate-4 w-full block shadow-2xl"
+          ></div>
         </section>
       </Card>
     </div>
