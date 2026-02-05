ce946b8e25bafd9456a927626c78cc23ae4cf164
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-05 11:57:38 +0100

    Uses zod validation - Checks for error ❌ and logs it into the console

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useState } from "react";
-import type { ContactFormTypes } from "@/validation/projectSchema";
+import {
+  contactFormSchema,
+  type ContactFormTypes,
+} from "@/validation/projectSchema";
+import z from "zod";
 
 function useForm() {
   const [messageData, setMessageData] = useState("");
+  const [errorMessage, setErrorMessage] = useState({});
   const [formData, setFormData] = useState<ContactFormTypes>({
     name: "",
     email: "",
@@ -26,13 +31,31 @@ function useForm() {
     }));
   };
 
-  const handleOnSubmit = (e: React.FormEvent) => {
+  const handleOnSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log({ ...formData, message: messageData });
+
+    try {
+      const result = contactFormSchema.safeParse({
+        ...formData,
+        message: messageData,
+      });
+
+      if (!result.success) {
+        setErrorMessage(result.error.issues);
+        console.log("Error message", result.error.issues);
+      }
+
+      console.log(result);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.log("Validation Fail", error.issues);
+      }
+    }
   };
   return {
     formData,
     messageData,
+    errorMessage,
     messageDataChange,
     handleChange,
     handleOnSubmit,
