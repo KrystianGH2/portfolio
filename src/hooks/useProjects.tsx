be7cea0bd76b438b699886b3595bfd8be1601d28
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-08 16:30:27 +0100

    Adds a loading state

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -3,25 +3,29 @@ import type { ProjectTypes } from "../types/types";
 
 function useProjects() {
   const [projects, setProjects] = useState<ProjectTypes[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const API_BASE =
     import.meta.env.VITE_API_BASE_URL || "https://portfolio-j42o.onrender.com";
 
   useEffect(() => {
     const fetchProjects = async () => {
+      setIsLoading(true);
       try {
         const res = await fetch(`${API_BASE}/api/projects`);
         const data = await res.json();
         setProjects(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchProjects();
   }, [API_BASE]);
 
-  return { projects };
+  return { projects, isLoading };
 }
 
 export default useProjects;
