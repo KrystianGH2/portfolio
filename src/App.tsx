bdaba0582be6ee4759595168dd39b00c9a006f88
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-08 14:41:29 +0100

    - Removes imported fetched projects - Adds Project component where it renders lists of projects - Renders footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
 "use client";
-import useProjects from "./hooks/useProjects";
-import type { ProjectTypes } from "./validation/projectSchema";
 import "./index.css";
 import Header from "./components/Header";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
 
 function App() {
-  const { projects } = useProjects();
-
-  console.log(projects);
-
-  const lists = projects.map((item: ProjectTypes, index: number) => {
-    return (
-      <div className="flex flex-col items-start" key={index}>
-        <img
-          className="w-80"
-          src={item?.imageUrl}
-          alt={`${item.title} image`}
-        />
-        <h1>{item.title}</h1>
-      </div>
-    );
-  });
   return (
     <>
-      <div className="w-full m-auto max-w-7xl">
+      <div className="w-full m-auto ">
         <Header />
         <About />
-        <section>{lists}</section>
+        <Projects />
         <Contact />
+        <Footer />
       </div>
     </>
   );
