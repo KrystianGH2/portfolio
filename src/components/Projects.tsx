cd8d7511b2f804dab11ed4f3cf7865e8a57e0cb9
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-08 14:55:13 +0100

    Maps the fetched data and renders it into a card

---
+++ src/components/Projects.tsx
@@ -0,0 +1,35 @@
+import useProjects from "@/hooks/useProjects";
+import type { ProjectTypes } from "@/validation/projectSchema";
+import { Card } from "./ui/card";
+
+function Projects() {
+  const { projects } = useProjects();
+
+  return (
+    <main id="projects" className="pt-36 m-auto w-full max-w-6xl px-6 lg:px-0">
+      <section className="flex flex-col justify-center items-center gap-3">
+        <span className="text-xs font-medium tracking-wide">PORTFOLIO</span>
+        <h1 className="font-bold text-3xl tracking-wide">Featured Projects</h1>
+      </section>
+      <section className="pt-15 gap-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {projects.map((item: ProjectTypes, index: number) => {
+          return (
+            <Card
+              className="flex flex-col p-6 items-start justify-center"
+              key={index}
+            >
+              <img
+                className="w-full max-w-60"
+                src={item?.imageUrl}
+                alt={`${item.title} image`}
+              />
+              <h1>{item.title}</h1>
+            </Card>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default Projects;
