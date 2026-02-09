6c9a05802afabb0e09bab3ea4d973298666ad7de
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-09 20:56:25 +0100

    Adds methods for deleting and finding projects by id

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://portfolio-roan-xi-45.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-roan-xi-45.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
@@ -33,6 +36,46 @@ app.post("/api/projects", async (req, res) => {
     await connectDB();
     const created = await Project.create(req.body);
     res.status(201).json(created.toObject());
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Internal Error", details: String(error) });
+  }
+});
+
+app.get("/api/projects/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await connectDB();
+
+    const projectById = await Project.findById(id).lean();
+
+    if (!projectById) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.json(projectById);
+  } catch (err) {
+    res.status(500).json({
+      message: "Server Error",
+      details: String(err),
+    });
+  }
+});
+
+app.delete("api/projects/:id", async (req, res) => {
+  try {
+    await connectDB();
+
+    const { id } = req.params;
+
+    const deleteProject = Project.findByIdAndDelete(id);
+
+    if (!deleteProject) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.json({ message: "Project deleted successfully" });
   } catch (error) {
     res
       .status(500)
