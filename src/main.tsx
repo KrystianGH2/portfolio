eb8b0bde169097e287418a5fb28c4b666b5face4
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-12 20:10:37 +0100

    Adds routes to dashboard pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./(pages)/admin/dashboard/index.tsx";
+import Form from "./components/Form.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/dashboard" element={<Dashboard />}>
+          <Route index element={<Form />} />
+          <Route path=":id" element={<div />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
