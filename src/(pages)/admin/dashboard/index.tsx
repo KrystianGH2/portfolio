b1fe7a4c1a5643eb47d01231683f44453b30cc65
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-12 20:11:21 +0100

    Renders the dashboard component where projects are fetched

--- src/(pages)/admin/dashboard/index.tsx
+++ src/(pages)/admin/dashboard/index.tsx
@@ -1,11 +1,11 @@
-import Form from "@/components/Form";
 import DashboardComponent from "@/components/Dashboard";
+import { Outlet } from "react-router-dom";
 function Dashboard() {
   return (
     <div className="w-full m-auto max-w-7xl">
       <h1 className="text-center">Dashboard Page</h1>
-      <DashboardComponent />
-      <Form />
+      <DashboardComponent/>
+      <Outlet/>
     </div>
   );
 }
