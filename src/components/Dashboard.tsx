d30506ad6f175368a0d8bbcb38fa90439100d5d2
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-12 20:09:41 +0100

    - Renders the fetched projects on the dashboard.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,17 @@
 import useProjects from "@/hooks/useProjects";
 import { Card } from "./ui/card";
+import { Link } from "react-router-dom";
 
 function DashboardComponent() {
   const { projects } = useProjects();
   return (
     <div className="max-w-6xl m-auto">
+      <h1>My Projects</h1>
       {projects.map((item) => (
         <Card className="p-4" key={item._id}>
-          <h2>{item.title}</h2>
+          <Link to={`/admin/dashboard/${item._id}`}>
+            <h2>{item.title}</h2>
+          </Link>
         </Card>
       ))}
     </div>
