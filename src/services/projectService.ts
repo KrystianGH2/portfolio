d5e11205ac74d01a274a3ae88272b5aae9dbac40
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-15 00:18:44 +0100

    - Moves the base url out of the functions - Makes an get request by id for fetching single projects

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,7 +1,8 @@
 import type { ProjectTypes } from "@/validation/projectSchema";
+const baseUrl =
+  import.meta.env.VITE_API_BASE_URL || "https://portfolio-j42o.onrender.com";
+
 export async function createProjects(payload: ProjectTypes) {
-  const baseUrl =
-    import.meta.env.VITE_API_BASE_URL || "https://portfolio-j42o.onrender.com";
   const options = {
     method: "POST",
     headers: {
@@ -24,8 +25,6 @@ export async function createProjects(payload: ProjectTypes) {
 }
 
 export async function updateProject(payload: ProjectTypes, id: string) {
-  const baseUrl =
-    import.meta.env.VITE_API_BASE_URL || "https://portfolio-j42o.onrender.com";
   const options = {
     method: "PUT",
     headers: {
@@ -43,6 +42,27 @@ export async function updateProject(payload: ProjectTypes, id: string) {
   } catch (error) {
     if (error instanceof Error) {
       throw new Error("Failed updating project.", error);
+    }
+  }
+}
+
+export async function getProjectById(id: string) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await fetch(`${baseUrl}/api/projects/${id}`, options);
+    if (!res.ok) {
+      throw new Error("Failed fetching data");
+    }
+
+    return res;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error("Failed fetching project.", error);
     }
   }
 }
