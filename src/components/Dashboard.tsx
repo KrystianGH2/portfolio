ff909750cafa17802947d52c4c5ccd313b3fdbda
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-16 11:48:13 +0100

    - Shows list of projects on mount - Simple ui for selected project

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import useProjects from "@/hooks/useProjects";
 import { Card } from "./ui/card";
 import { Link } from "react-router-dom";
 
 function DashboardComponent() {
   const { projects } = useProjects();
+  const [selected, setSelected] = useState<string | null>(null);
+
   return (
-    <div className="max-w-6xl m-auto">
-      <h1>My Projects</h1>
-      {projects.map((item) => (
-        <Card className="p-4" key={item._id}>
-          <Link to={`/admin/dashboard/${item._id}`}>
-            <h2>{item.title}</h2>
-          </Link>
-        </Card>
-      ))}
+    <div className="max-w-6xl w-full  lg:w-1/2 ">
+      <h1 className="font-bold text-3xl tracking-wide">My Projects</h1>
+
+      <section className="grid grid-cols-1">
+        {projects.map((item) => (
+          <Card
+            key={item._id}
+            onClick={() => setSelected(item._id)}
+            className={`p-4 transition w-full relative ${
+              selected === item._id ? "border-blue-500" : "border"
+            }  my-2 `}
+          >
+            <Link to={item._id}>
+              <h2>{item.title}</h2>
+            </Link>
+            <span
+              className={` ${
+                selected === item._id
+                  ? "block w-3 h-3 rounded-full bg-black "
+                  : ""
+              } absolute right-5 top-5.5 animate-pulse `}
+            ></span>
+          </Card>
+        ))}
+      </section>
     </div>
   );
 }
