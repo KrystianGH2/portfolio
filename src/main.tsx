50e65c95294b0b4d380656b0eb8a0ddb621ca739
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-24 17:40:16 +0100

    feat(auth): protects admin dashboard route with ProtectedRoute

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./(pages)/admin/dashboard/index.tsx";
 import Form from "./components/Form.tsx";
 import AdminLogin from "./(pages)/admin/login/index.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin/dashboard" element={<Dashboard />}>
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Form />} />
           <Route path=":id" element={<Form />} />
         </Route>
