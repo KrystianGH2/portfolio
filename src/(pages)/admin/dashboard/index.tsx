44a6d554d2725125ac86f7f06778db2f80225601
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-02-24 17:37:22 +0100

    feat(admin): Adds the logout ui into Dashboard page

--- src/(pages)/admin/dashboard/index.tsx
+++ src/(pages)/admin/dashboard/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "@/services/authService";
+import LogoutButton from "@/components/LogoutButton";
 function Dashboard() {
   const nav = useNavigate();
 
@@ -14,9 +15,12 @@ function Dashboard() {
   }, [nav]);
   return (
     <div className="w-full m-auto max-w-6xl px-5">
-      <h1 className="text-baseline font-bold text-3xl py-5 m-auto tracking-wide">
-        Dashboard Page
-      </h1>
+      <div className="flex items-center justify-between ">
+        <h1 className="text-baseline font-bold text-3xl py-5 tracking-wide">
+          Dashboard Page
+        </h1>
+        <LogoutButton />
+      </div>
       <div className="flex flex-col lg:flex-row  gap-8">
         <DashboardComponent />
         <Outlet />
