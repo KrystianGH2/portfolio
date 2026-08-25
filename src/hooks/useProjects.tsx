5fbacb997c1d85dccd3166f646e8e9a3e6241f13
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2026-08-25 21:36:21 +0200

    fix(error message): Throws an error status when res is not ok

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -6,12 +6,18 @@ const API_BASE =
 function useProjects() {
   const [projects, setProjects] = useState<Project[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState("");
 
   useEffect(() => {
     const fetchProjects = async () => {
       setIsLoading(true);
       try {
         const res = await fetch(`${API_BASE}/api/projects`);
+        console.log(res);
+
+        if (!res.ok) {
+          setIsError(res.statusText || "Failed to fetch projects.");
+        }
         const data = await res.json();
         setProjects(data);
       } catch (error) {
@@ -24,7 +30,7 @@ function useProjects() {
     fetchProjects();
   }, []);
 
-  return { projects, isLoading };
+  return { projects, isLoading, isError };
 }
 
 export default useProjects;
